a471f5ee02ff746d1f09cd57e436bafacb7a8b52
blakepalmer <blake_palmer28@milton.edu>
2026-04-17 08:56:15 -0400

    edits

--- src/Fish2.java
+++ src/Fish2.java
@@ -16,8 +16,6 @@ public class Fish2 {
     public boolean isCrashing; //a boolean to denote if the character is crashing or not
 
 
-//fish2 java
-
 
     public Fish2(int pXpos, int pYpos) {
         xpos = pXpos;
